ee346ac314dcf28a9151d12db7919154fb2a2e6b
abdyus <166007645+abdyus@users.noreply.github.com>
2025-06-17 18:21:50 +0530

    allowed \n as a delimiter

--- calc.py
+++ calc.py
@@ -3,6 +3,8 @@
 def add(numbers):
     if numbers == "":
         return 0
-    parts = numbers.split(",")
+    
+    commas = numbers.replace("\n",",")
+    parts = commas.split(",")
     nums = [int(p) for p in parts if p.strip() != ""]
     return sum(nums)
